52e74ae876fee21fb73528139fe266e9d4ffe0d9
Yasar khan <yasarkhan@Mac.lan>
2025-09-24 17:42:28 +0530

    as

--- index.js
+++ index.js
@@ -7,12 +7,26 @@ app.use(bodyParser.json());
 app.use(cors())
 
 const login = require('./routes/student/login')
+const admins = require('./routes/admin/index')
+
+
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../../credentials/fire.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ems-vmv-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
 
 const NodeCache = require('node-cache'); // Import the node-cache library
 const myCache = new NodeCache({ stdTTL: 60 });
 
 
 app.use('/student',login)
+app.use('/admin',admins)
+
 
 app.listen(3000,()=>{
 console.log('listening...')
