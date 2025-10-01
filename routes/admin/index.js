d291b042e43ffae2e6cf667a4e38bff588ece0d7
Yasar khan <yasarkhan@Mac.lan>
2025-10-01 20:05:26 +0530

    as

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -42,7 +42,7 @@ app.post('/login', async (req, res) => {
  
         let token = jwt.sign({ email,access:admin.access}, KEY, { expiresIn: '1h' });
 
-       return res.status(200).send({token});
+       return res.status(200).send({token,email,access:admin.access});
     } catch (error) {
         console.error('Login error:', error);
         res.status(500).send({ message: 'An error occurred during login.' });
@@ -211,6 +211,128 @@ app.get('/approvePayment',async (req, res) => {
    
 });
 
+
+app.post('/approveApplication',async (req, res) => {
+
+    const  body=req.body
+    const data = require('../../data/allAdmin.json');
+ 
+    try{
+        let tok = jwt.verify(req.query?.token,KEY)
+
+        if(!data[tok.email] || !body)
+            return    res.status(500).send({ message: 'Unrestricted !!' });
+
+        // console.log(req.body)
+
+        let count  = (await admin.firestore().collection('applications').where('status','==','APPROVED').where('type','==',body.type).where('semester','==',body.semester).where('exam','==',body.exam).count().get()).data().count
+        
+        let obj
+        seq.forEach(el=>{
+            if(el.scheme==body.type && el.type==body.exam && el.semester==body.semester){
+                obj=el
+            }
+        })
+
+        var rollNo=count+obj?.startsFrom ||0
+
+        
+
+        let q =await admin.firestore().doc('applications/'+body.id).set({...body,approvedBy:tok.email,approvedOn:Date.now(),status:'APPROVED',rollNo},{merge:true})
+
+       return  res.status(200).send({message:'Field updated !!',rollNo});
+    }catch(err){
+        console.log(err)
+      return  res.status(400).send({ message: 'Token expired !!' });
+
+    }
+   
+});
+
 app.use(express.json({ limit: '100mb' }));
 
 module.exports = app;
+
+const seq=[{
+    scheme:'BA-CBCS',semester:1,type:'Backlog', startsFrom:220001
+  },{
+    scheme:'BA-CBCS',semester:2,type:'Backlog', startsFrom:220051
+  },{
+    scheme:'BA-CBCS',semester:3,type:'Backlog', startsFrom:220081
+  },{
+    scheme:'BA-CBCS',semester:4,type:'Backlog', startsFrom:220131
+  },{
+    scheme:'BA-CBCS',semester:5,type:'Backlog', startsFrom:220191
+  },{
+    scheme:'BA-CBCS',semester:6,type:'Backlog', startsFrom:220301
+  },{
+    scheme:'BSC-CBCS',semester:1,type:'Backlog', startsFrom:220451
+  },{
+    scheme:'BSC-CBCS',semester:2,type:'Backlog', startsFrom:220511
+  },{
+    scheme:'BSC-CBCS',semester:3,type:'Backlog', startsFrom:220571
+  },{
+    scheme:'BSC-CBCS',semester:4,type:'Backlog', startsFrom:220681
+  },{
+    scheme:'BSC-CBCS',semester:5,type:'Backlog', startsFrom:220801
+  },{
+    scheme:'BSC-CBCS',semester:6,type:'Backlog', startsFrom:220901
+  },
+  ,{
+    scheme:'BSCHS-CBCS',semester:1,type:'Backlog', startsFrom:221051
+  },{
+    scheme:'BSCHS-CBCS',semester:2,type:'Backlog', startsFrom:221056
+  },{
+    scheme:'BSCHS-CBCS',semester:3,type:'Backlog', startsFrom:221061
+  },{
+    scheme:'BSCHS-CBCS',semester:4,type:'Backlog', startsFrom:221070
+  },{
+    scheme:'BSCHS-CBCS',semester:5,type:'Backlog', startsFrom:221080
+  },{
+    scheme:'BSCHS-CBCS',semester:6,type:'Backlog', startsFrom:221086
+  }
+  ,{
+    scheme:'UG-NEP-OLD',semester:1,type:'Backlog', startsFrom:221091
+  },{
+    scheme:'UG-NEP-OLD',semester:2,type:'Backlog', startsFrom:221171
+  },{
+    scheme:'UG-NEP-OLD',semester:3,type:'Backlog', startsFrom:221251
+  },{
+    scheme:'UG-NEP-OLD',semester:4,type:'Backlog', startsFrom:221351
+  },{
+    scheme:'UG-NEP-OLD',semester:5,type:'Regular', startsFrom:111401
+  },
+  ,{
+    scheme:'UG-NEP-NEW',semester:1,type:'Regular', startsFrom:110001
+  },
+  {
+    scheme:'UG-NEP-NEW',semester:1,type:'Backlog', startsFrom:221551
+  },
+  {
+    scheme:'UG-NEP-NEW',semester:2,type:'Backlog', startsFrom:221851
+  },
+  
+  ,{
+    scheme:'UG-NEP-NEW',semester:3,type:'Regular', startsFrom:110801
+  },
+  {
+    scheme:'PG-NEP',semester:1,type:'Regular', startsFrom:111901
+  },
+  ,
+  {
+    scheme:'PG-NEP',semester:1,type:'Backlog', startsFrom:222151
+  },
+  {
+    scheme:'PG-NEP',semester:2,type:'Backlog', startsFrom:222301
+  },
+  
+  {
+    scheme:'PG-NEP',semester:3,type:'Regular', startsFrom:112351
+  },
+  {
+    scheme:'PG-NEP',semester:3,type:'Backlog', startsFrom:222351
+  },
+  {
+    scheme:'PG-NEP',semester:4,type:'Backlog', startsFrom:222451
+  },
+  ]
